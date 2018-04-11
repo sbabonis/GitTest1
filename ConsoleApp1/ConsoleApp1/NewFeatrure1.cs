85faac06eb94878aeb7a23dcb3bde917f19fed29
steve <steve@avitarassociates.com>
2018-04-11 16:39:07 -0400

    refactor add

--- ConsoleApp1/ConsoleApp1/NewFeatrure1.cs
+++ ConsoleApp1/ConsoleApp1/NewFeatrure1.cs
@@ -2,11 +2,8 @@
 
 public class Feature1
 {
-	public int Add()
-	{
-        int x1 = 1;
-        int x2 = 2;
-        int sum = x1 + x2;
-        return sum;
+	public int Add(int x1, int x2)
+    { 
+        retur x1 + x2;
 	}
 }
