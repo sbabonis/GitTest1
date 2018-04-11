eeb107ab818e2643943d10a9ea736a92bbf15271
steve <steve@avitarassociates.com>
2018-04-11 15:52:35 -0400

    fixed type

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             //this code changed.
-            //code was changed in V.asdfadfasdf
+            //code was changed in VS
 
         }
     }
