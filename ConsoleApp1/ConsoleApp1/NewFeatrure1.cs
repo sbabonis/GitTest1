5ba8db8bcf43316e92ca6af8a6787c1bd79371ac
steve <steve@avitarassociates.com>
2018-04-11 16:35:32 -0400

    Add

--- ConsoleApp1/ConsoleApp1/NewFeatrure1.cs
+++ ConsoleApp1/ConsoleApp1/NewFeatrure1.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 
-public class Class1
+public class Feature1
 {
-	public Class1()
+	public int Add()
 	{
+        int x1 = 1;
+        int x2 = 2;
+        int sum = x1 + x2;
+        return sum;
 	}
 }
