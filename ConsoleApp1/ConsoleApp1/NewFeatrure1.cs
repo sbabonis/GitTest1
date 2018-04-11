032526c764b628ae52ba6e65008b82dc7f9f1e9f
sbabonis <38291492+sbabonis@users.noreply.github.com>
2018-04-11 16:37:06 -0400

    Update NewFeatrure1.cs

--- ConsoleApp1/ConsoleApp1/NewFeatrure1.cs
+++ ConsoleApp1/ConsoleApp1/NewFeatrure1.cs
@@ -4,9 +4,9 @@ public class Feature1
 {
 	public int Add()
 	{
-        int x1 = 1;
-        int x2 = 2;
-        int sum = x1 + x2;
+        var int x1 = 1;
+        var int x2 = 2;
+        var int sum = x1 + x2;
         return sum;
 	}
 }
